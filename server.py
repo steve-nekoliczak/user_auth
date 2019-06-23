e30d3a00c6d7f048ddc4dfec9017f75d5a756bca
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-06-23 18:34:46 -0500

    Adding argument for debug mode.

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ def get_args():
     ap = argparse.ArgumentParser('Handle user data for DieFehlendeWoerter.')
 
     # Add args
+    ap.add_argument('-d', '--debug',
+                    action='store_true',
+                    help="Enable debugging mode.")
+
     ap.add_argument('-p', '--port', type=int,
                     help="Port number to run this service on.",
                     default=5014)
@@ -19,5 +23,5 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
 
-    connex_app.run(debug=True, port=args.port)
+    connex_app.run(debug=args.debug, port=args.port)
 
