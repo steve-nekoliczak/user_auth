7ea827cef455aa9076d49d66b5b8b72f41cc7919
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2019-06-26 18:21:04 -0500

    Fixing broken wsgi.py file

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,16 @@
 import os
-from server import connex_app as application
+
+import config
 from server import get_args
 
 activate_this = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'venv', 'bin', 'activate_this.py')
 
+application = config.connex_app
+
 if __name__ == "__main__":
     args = get_args()
+
     with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
 
