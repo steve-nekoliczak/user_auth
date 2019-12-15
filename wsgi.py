37fb20f01f5ed4572514e73cdcb063f85e28449f
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2019-12-15 13:32:00 -0600

    adding uwsgi code

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 import config
 from args import get_args
 
+app = config.connex_app
+
 
 if __name__ == "__main__":
     args = get_args()
-    config.connex_app.run(debug=args.debug, port=args.port)
+    app.run(debug=args.debug, port=args.port)
